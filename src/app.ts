9776a6901ce9fb76dc6728f8949bd668dbd5de85
PhanKiet <tuankietvn@outlook.com>
2020-10-20 07:28:01 +0700

    Adding stop, pause, resume commands

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import Discord, { Client } from 'discord.js';
+import Discord, { Channel, Client, Emoji } from 'discord.js';
 import ytdl from 'ytdl-core-discord';
 
 import * as dotenv from 'dotenv';
@@ -13,10 +13,12 @@ const __main__ = () =>
     const app: Client = new Client();
     const youtube: YouTube = new YouTube(process.env.YOUTUBE_TOKEN);
 
-    let curStep = 0;
+    let song_choose = false;
+    let song_pause = false;
     let vids: Video[];
     let playlist: Video[] = [];
     let connection: Discord.VoiceConnection;
+    let channel: Discord.VoiceChannel;
 
     app.login(process.env.DISCORD_TOKEN);
     
@@ -29,79 +31,118 @@ const __main__ = () =>
     {
         if (!message.author.bot && message.content.startsWith(process.env.PREFIX))
         {
-            const data = message.content.trim().substring(1);
+            const data = message.content.trim().substring(1).toLowerCase();
 
-            if (curStep === 0)
+            if (data.startsWith(process.env.COMMAND_PLAY))
             {
-                if (data.startsWith(process.env.COMMAND_PLAY))
+                channel = message.member.voice.channel;
+
+                await message.react(process.env.OK_SYMBOL);
+
+                // Check if user in voice channel or bot have enough permission to play music
+                if (!channel)
                 {
-                    const channel = message.member.voice.channel;
+                    await message.channel.send("You need to be in voice channel to play music.");
+                    return;
+                }
+                if (!channel.permissionsFor(message.client.user).has("CONNECT"))
+                {
+                    await message.channel.send("I don\'t have permission to join the voice channel");
+                    return;
+                }
+                if (!channel.permissionsFor(message.client.user).has("SPEAK"))
+                {
+                    await message.channel.send("I don\'t have permission to speak in the voice channel");
+                    return;
+                }
 
-                    // Check if user in voice channel or bot have enough permission to play music
-                    if (!channel)
+                connection = await channel.join();
+
+                const query = data.substring((process.env.PREFIX + process.env.COMMAND_PLAY).length + 1);
+
+                if (!isEmpty(query))
+                {
+                    vids = (await youtube.searchVideos(query,  parseInt(process.env.NUMBER_OF_YOUTUBE_VIDS))).results;
+
+                    if (isEmpty(vids))
                     {
-                        await message.channel.send("You need to be in voice channel to play music.");
-                        return;
-                    }
-                    if (!channel.permissionsFor(message.client.user).has("CONNECT"))
-                    {
-                        await message.channel.send("I don\'t have permission to join the voice channel");
-                        return;
-                    }
-                    if (!channel.permissionsFor(message.client.user).has("SPEAK"))
-                    {
-                        await message.channel.send("I don\'t have permission to speak in the voice channel");
-                        return;
-                    }
-
-                    connection = await channel.join();
-                    console.log(connection);
-                    
-
-                    const query = data.substring((process.env.PREFIX + process.env.COMMAND_PLAY).length + 1);
-
-                    if (!isEmpty(query))
-                    {
-                        vids = (await youtube.searchVideos(query,  parseInt(process.env.NUMBER_OF_YOUTUBE_VIDS))).results;
-
-                        if (isEmpty(vids))
-                        {
-                            await message.channel.send("No video found, please try again");
-                        }
-                        else
-                        {
-                            let title_list = "Which one ?\n";
-                            vids.forEach((vid, id) =>
-                            {
-                                title_list += (id + 1) + '. ' + vid.title + '\n';
-                            });
-                            ++ curStep;
-                            await message.channel.send(title_list);
-
-                        }
+                        await message.channel.send("No video found, please try again");
                     }
                     else
                     {
-                        await message.channel.send(`Use ${process.env.PREFIX + process.env.COMMAND_PLAY} + <song name> to play a song`);
+                        let title_list = "Which one ?\n";
+                        vids.forEach((vid, id) =>
+                        {
+                            title_list += (id + 1) + '. ' + vid.title + '\n';
+                        });
+                        song_choose = true;
+                        await message.channel.send(title_list);
+
+                    }
+                }
+                else
+                {
+                    await message.channel.send(`Use ${process.env.PREFIX + process.env.COMMAND_PLAY} + <song name> to play a song`);
+                }
+            }
+            else if (song_choose)
+            {
+                song_choose = false;
+                if ("1" <= data && data <= process.env.NUMBER_OF_YOUTUBE_VIDS)
+                {
+                    try
+                    {
+                        const url = `https://youtube.com/watch?v=${vids[parseInt(data) - 1].id}`;
+
+                        await message.react(process.env.OK_SYMBOL);
+
+                        playlist.push(await youtube.getVideo(url));
+
+                        connection.play(await ytdl(url), {type: 'opus'});
+
+                        await message.channel.send(`Now playing ${vids[parseInt(data) - 1].title}}...`);
+                    }
+                    catch
+                    {
+                        await message.channel.send("Something bad happened, please try again.");
+                    }
+                }
+                else
+                {
+                    await message.react(process.env.THINK_SYMBOL);
+                    await message.channel.send(`Bro ???`);
+                }
+            }
+            else
+            {
+                if (data.startsWith(process.env.COMMAND_STOP))
+                {
+                    await message.react(process.env.SAD_SYMBOL);
+                    connection.dispatcher.end();
+                    channel.leave();
+                }
+                else if (data.startsWith(process.env.COMMAND_PAUSE))
+                {
+                    await message.react(process.env.OK_SYMBOL);
+                    connection.dispatcher.pause(true);
+                    song_pause = true;
+                }
+                else if (data.startsWith(process.env.COMMAND_RESUME))
+                {
+                    if (song_pause)
+                    {
+                        await message.react(process.env.OK_SYMBOL);
+                        connection.dispatcher.resume();
+                    }
+                    else
+                    {
+                        await message.react(process.env.THINK_SYMBOL);
+                        await message.channel.send(`Bro ???`);
                     }
                 }
             }
-            else if (curStep === 1)
-            {
-                if ("1" <= data && data <= process.env.NUMBER_OF_YOUTUBE_VIDS)
-                {
-                    ++ curStep;
-                    const url = `https://youtube.com/watch?v=${vids[parseInt(data) - 1].id}`;
 
-                    playlist.push(await youtube.getVideo(url));
-
-                    connection.play(await ytdl(url), {type: 'opus'});
-                }
-                else curStep = 0;
-            }
-            else curStep = 0;
-
-            console.log(curStep);
+            console.log(song_choose);
         }
     });
 
